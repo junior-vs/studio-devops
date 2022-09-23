92d554ceaa82bb70bf509f48ba71c9fc9b08cd8f
Valdir Junior <valdir.junior.dev@gmail.com>
2022-09-23 19:21:25 -0300

    reboot

---
+++ docker/enterprise_enviroment/lab/scritps/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+  CREATE USER sonarapp;
+  CREATE USER keycloakapp;
+
+  CREATE DATABASE sonardb;
+  CREATE DATABASE keycloakdb;
+
+  ALTER USER sonarapp PASSWORD 'sonarapp';
+  GRANT ALL PRIVILEGES ON DATABASE sonardb TO sonarapp;
+  
+  ALTER USER keycloakapp PASSWORD 'keycloakapp';  
+  GRANT ALL PRIVILEGES ON DATABASE keycloakdb TO keycloakapp;
+  
+EOSQL
